28a999d6612d9570b158ffb938ec86cddf0d43ac
Ahmad Hamido <letraingrand1@gmail.com>
2022-11-23 10:57:48 +0100

    updated the endGame function

--- script.js
+++ script.js
@@ -15,7 +15,19 @@ function attack() {
 }
 
 function endGame() {
-    console.log('Game Over!');
+    const endMessage = wizard.health === 0 && orc.health === 0 ?
+        "No victors - all creatures are dead" :
+        wizard.health > 0 ? "The Wizard Wins" :
+        "The Orc is Victorious"
+    
+    const endEmoji = orc.dead ? "🔮" : "☠️";
+    
+    document.body.innerHTML = `
+            <div class="end-game">
+                <h2>Game Over</h2>
+                <h3>${endMessage}</h3>
+                <p class="end-emoji">${endEmoji}</p>
+            </div>`
 }
 
 function render() {
